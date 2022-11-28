e8fb57600db51342a710572b2ce4f8c73139f7a1
Arman Khan <sparmankhan@gmail.com>
2022-11-28 19:42:06 +0600

    delete product from mongodb

--- index.js
+++ index.js
@@ -112,6 +112,14 @@ const run = async () => {
       res.send(result);
     });
 
+
+    app.delete('/products/:id',async(req,res)=>{
+      const id = req.params.id
+      const query = {_id : ObjectId(id)}
+      const result = await  productsCollection.deleteOne(query)
+      res.send(result)
+    })
+
    
 
     app.get("/payproducts/:id", async (req, res) => {
